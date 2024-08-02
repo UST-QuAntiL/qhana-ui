ccb9390825e299a155b4d9da3fe8a85456924af4
Fabian Bühler <fabian.buehler@iaas.uni-stuttgart.de>
2024-08-02 12:12:14 +0200

    Fix plugin filter editor error on empty string inputs

--- src/app/components-small/plugin-filter-editor/plugin-filter-editor.component.ts
+++ src/app/components-small/plugin-filter-editor/plugin-filter-editor.component.ts
@@ -40,6 +40,9 @@ export class PluginFilterEditorComponent implements OnInit {
         }
         this.registry.getByApiLink<TemplateTabApiObject>(this.tabLink).then(response => {
             this.filterString = response?.data?.filterString ?? this.filterString;
+            if (!this.filterString) {
+                this.filterString = "{}";
+            }
             this.filterObject = JSON.parse(this.filterString);
             this.updateFilter(this.filterObject);
         });
