7532f4cd76dad7b86be01f2ff6caf5a759f3fefc
Philipp Wundrack <philipp.wundrack@live.de>
2025-03-27 09:27:48 +0100

    fix mimetype matching in choose data dialog

--- src/app/components-small/plugin-preview/plugin-preview.component.ts
+++ src/app/components-small/plugin-preview/plugin-preview.component.ts
@@ -2,19 +2,7 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ApiLink } from 'src/app/services/api-data-types';
 import { isPluginApiObject, PluginApiObject } from 'src/app/services/qhana-api-data-types';
 import { PluginRegistryBaseService } from 'src/app/services/registry.service';
-
-
-/**
- * Escape a string to be used inside a regex as literal.
- *
- * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping
- *
- * @param string string to escape
- * @returns escaped string
- */
-function escapeRegExp(string: string) {
-    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-}
+import { getMimetypeLikeMatcher } from 'src/app/utils';
 
 
 @Component({
@@ -96,8 +84,8 @@ export class PluginPreviewComponent implements OnChanges {
     }
 
     private getParameter(plugin: PluginApiObject, dataType: string | null, contentType: string | null) {
-        const dataTypeMatcher = this.getMimetypeLikeMatcher(dataType);
-        const contentTypeMatcher = this.getMimetypeLikeMatcher(contentType);
+        const dataTypeMatcher = getMimetypeLikeMatcher(dataType);
+        const contentTypeMatcher = getMimetypeLikeMatcher(contentType);
 
         let param: string | null = null;
         let required: boolean = false;
@@ -127,57 +115,4 @@ export class PluginPreviewComponent implements OnChanges {
         });
         return param;
     }
-
-    private getMimetypeLikeMatcher(mimetypeLike: string | null) {
-        if (mimetypeLike == null || mimetypeLike === "*" || mimetypeLike === "*/*") {
-            // no filter/wildcard => match everything
-            return (toMatch: string) => true;
-        }
-        if (!mimetypeLike.includes("/")) {
-            // only one part filter
-            return (toMatch: string) => {
-                if (toMatch === "*" || toMatch.startsWith("*/")) {
-                    return true;  // matching string is wildcard
-                }
-                if (toMatch === mimetypeLike || toMatch.startsWith(`${mimetypeLike}/`)) {
-                    return true;  // actual match
-                }
-                return false;
-            }
-        }
-
-        // mimetype like contains both parts
-        const splitType = mimetypeLike.split("/", 2);
-        const splitStart = splitType[0];
-        let splitEnd = splitType[1];
-        if (splitEnd == null || splitEnd === "") {
-            splitEnd = "*";
-        }
-
-        let regex = "^";
-        if (splitStart == null || splitStart === "" || splitStart === "*") {
-            regex += "[^/]+";
-        } else {
-            regex += `(${escapeRegExp(splitStart)}|\\*)`;
-        }
-        regex += "/";
-        if (splitEnd == null || splitEnd === "" || splitEnd === "*") {
-        } else {
-            regex += `(${escapeRegExp(splitEnd)}|\\*)`;
-        }
-
-        const compiledRegex = new RegExp(regex);
-
-
-        return (toMatch: string) => {
-            if (toMatch === "*" || toMatch.startsWith("*/*")) {
-                return true;  // matching string is wildcard
-            }
-            if (compiledRegex.test(toMatch)) {
-                return true;
-            }
-            return false;
-        }
-    }
-
 }
