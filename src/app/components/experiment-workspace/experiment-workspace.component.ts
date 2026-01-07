d254671a79ed25794a751d39e42524d9c697e0dc
MaxSilvester <138241982+MaxSilvesterQWE@users.noreply.github.com>
2026-01-07 02:55:09 +0100

    only care about plugin name if pluginId is null

--- src/app/components/experiment-workspace/experiment-workspace.component.ts
+++ src/app/components/experiment-workspace/experiment-workspace.component.ts
@@ -66,7 +66,7 @@ export class ExperimentWorkspaceComponent implements OnInit, OnDestroy {
 
             let pluginId = params.get('plugin');
             const pluginName = params.get('plugin-name');
-            if (pluginName != null){
+            if (pluginId == null && pluginName != null){
                 const pluginPage = await this.registry.getByRel<CollectionApiObject>([["plugin", "collection"]], new URLSearchParams({ }), true);
 
                 pluginPage?.data?.items?.forEach(item => {
