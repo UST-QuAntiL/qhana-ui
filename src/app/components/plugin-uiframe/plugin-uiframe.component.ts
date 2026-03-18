a0ddb2b61395f5ecc1b5cf421871558122cf230e
MaxSilvester <138241982+MaxSilvesterQWE@users.noreply.github.com>
2026-03-18 15:59:48 +0100

    switch-plugin: adjust code for possible nulls

--- src/app/components/plugin-uiframe/plugin-uiframe.component.ts
+++ src/app/components/plugin-uiframe/plugin-uiframe.component.ts
@@ -571,7 +571,7 @@ export class PluginUiframeComponent implements OnChanges, OnDestroy {
             console.error(`no plugin with name ${request.pluginName} found!`);
             return;
         }
-        const pluginId = plugins.data.items[0].resourceKey.pluginId;
+        const pluginId = plugins?.data.items[0].resourceKey?.pluginId;
         this.router.navigate(
             ['/experiments', this.experimentId, 'temp', pluginId ],
             { queryParams : queryParams}
