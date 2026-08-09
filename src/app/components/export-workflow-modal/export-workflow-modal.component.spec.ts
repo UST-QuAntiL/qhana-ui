d95b3c4b58a04d8586784b4d097e0ad409db123f
st168924 <Sebastian@bruy.de>
2026-08-09 03:11:13 +0200

    Fix export workflow modal test

--- src/app/components/export-workflow-modal/export-workflow-modal.component.spec.ts
+++ src/app/components/export-workflow-modal/export-workflow-modal.component.spec.ts
@@ -1,4 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
 
 import { ExportWorkflowModalComponent } from './export-workflow-modal.component';
 
@@ -8,10 +18,34 @@ describe('ExportWorkflowModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ExportWorkflowModalComponent]
+      declarations: [
+        ExportWorkflowModalComponent
+      ],
+      imports: [
+        FormsModule,
+        MatButtonModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatListModule,
+        MatSelectModule
+      ],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            steps: []
+          }
+        },
+        {
+          provide: MatDialogRef,
+          useValue: {
+            close: () => {}
+          }
+        }
+      ]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(ExportWorkflowModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
